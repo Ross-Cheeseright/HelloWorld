6a9d352ca0c5570b1b6b022635cd876cd4825ae6
Ross Cheeseright <r.cheeseright@gmail.com>
2018-01-22 12:53:18 +0000

    Added a textView to the main view.

--- app/src/main/java/com/twobeardedguys/ross/helloworld/MainActivity.java
+++ app/src/main/java/com/twobeardedguys/ross/helloworld/MainActivity.java
@@ -2,12 +2,20 @@ package com.twobeardedguys.ross.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.R.attr.id;
+import static android.R.id.text1;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText("Goodbye world!");
+
     }
 }
